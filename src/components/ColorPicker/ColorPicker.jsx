6bb0fbcc2ee9aa382a74e7e95a7ae090114b32c0
Алексей Савельев <mtptz21@gmail.com>
2023-02-24 14:33:19 +0300

    refactor: change colorpicker part

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -1,58 +1,36 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 // Библиотека classNames сейчас используется с scss, усли нужно использовать библиотеку с css-modules то смотреть документацию на сайте(пример: import classNames=require('classnames/bind') дальше по инструкции)
 import classNames from 'classnames';
 import './ColorPicker.scss';
 
-class ColorPicker extends Component {
-  state = {
-    activeOptionIdx: 3,
-  };
+export default function ColorPicker({ options }) {
+  const [activeOptionIdx, setActiveOptionIdx] = useState(0);
 
-  //Метод получает индекс кнопку(option) на которую нажимаем и просто кидает его в state
-  setActiveIdx = index => {
-    this.setState({ activeOptionIdx: index });
-  };
+  // с помощью индекса берем label у активной кнопки и выводим в параграфе(это называется вычисляемое свойство на базе пропов и state) / берем из массива по индексу обьект и из обьект деструктуризируем свойство label
+  const { label } = options[activeOptionIdx];
 
-  makeOptionClassName = index => {
+  const makeOptionClassName = index => {
     // Добавляем класс по условию с помощью БИБЛИОТЕКИ classNames(первым идет дефолтные стили(сколько угодно штук, потом в обьекте идут те которые нужно добавить, а потом условие при котором нужно добавить))
     return classNames('ColorPicker-option', {
-      'ColorPicker-option-active': index === this.state.activeOptionIdx,
+      'ColorPicker-option-active': index === activeOptionIdx,
     });
-
-    // Тут описано добавление класса по условию БЕЗ библиотеки classNames
-    // const optionClasses = ['ColorPicker-option'];
-    // if (index === this.state.activeOptionIdx) {
-    //   optionClasses.push('ColorPicker-option-active');
-    // }
-    // return optionClasses.join(' ');
   };
 
-  render() {
-    // Деструктуризируем что бы убрать this.state и this.proprs что бы разметка была чище
-    const { activeOptionIdx } = this.state;
-    const { options } = this.props;
-
-    // с помощью индекса берем label у активной кнопки и выводим в параграфе(это называется вычисляемое свойство на базе пропов и state)
-    const { label } = options[activeOptionIdx];
-
-    return (
-      <div className="ColorPicker">
-        <h2 className="ColorPicker-title">Color Picker</h2>
-        <p>Выбрать цвет: {label}</p>
-        <div>
-          {options.map(({ label, color }, index) => (
-            <button
-              key={label}
-              className={this.makeOptionClassName(index)}
-              style={{ backgroundColor: color }}
-              // Передаем ссылку на функцию в onClick, index доступен через замыкание
-              onClick={() => this.setActiveIdx(index)}
-            ></button>
-          ))}
-        </div>
+  return (
+    <div className="ColorPicker">
+      <h2 className="ColorPicker-title">Color Picker</h2>
+      <p>Выбрать цвет: {label}</p>
+      <div>
+        {options.map(({ label, color }, index) => (
+          <button
+            key={label}
+            className={makeOptionClassName(index)}
+            style={{ backgroundColor: color }}
+            // Передаем ссылку на функцию в onClick, index доступен через замыкание
+            onClick={() => setActiveOptionIdx(index)}
+          ></button>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default ColorPicker;
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,113 +1,97 @@
-import React, { Component } from 'react';
-import './Form.scss';
+// import { useState } from 'react';
+// import './Form.scss';
 
-class Form extends Component {
-  state = {
-    name: '',
-    secondName: '',
-    licence: false,
-    experience: 'Junior',
-  };
+// export default function Form() {
+//   const [name, setName] = useState('');
+//   const [secondName, setSecondname] = useState('');
+//   const [licence, setLicence] = useState(false);
+//   const [experience, setExperience] = useState('Junior');
 
-  // Форма, используем вычисляемые свойство обьекта(например: const a='qwe'-Пишем: [a]:5-получаем {qwe:5}). Сделали универсальный метод, каждый инпут получает то что мы вводим
-  handleChange = e => {
-    this.setState({
-      [e.currentTarget.name]: e.currentTarget.value,
-    });
-  };
+//   const handleChange = e => {
+//     switch (e.target.name) {
+//       case 'name':
+//         setName(e.target.value);
+//         break;
+//       case 'secondName':
+//         setSecondname(e.target.value);
+//         break;
+//       case 'licence':
+//         setLicence(e.target.value);
+//         break;
+//       case 'experience':
+//         setExperience(e.target.value);
+//         break;
 
-  // для Сабмита формы делаем отдельный метод
-  handleSubmit = e => {
-    e.preventDefault();
-    // Получаем данные из формы при сабмите
-    this.props.giveMeData(this.state);
-    // Вызываем метод reset для очистки формы после сабмита
-    this.reset();
-  };
+//       default:
+//         return;
+//     }
+//   };
 
-  // метод для чекбокса
-  handleLicence = e => {
-    this.setState({ licence: e.currentTarget.checked });
-  };
-  // метод для очистки формы после сабмита
-  reset = () => {
-    this.setState({ name: '', secondName: '' });
-  };
+//   return (
+//     <form onSubmit={this.handleSubmit}>
+//       <label>
+//         Имя{' '}
+//         <input type="text" name="name" value={name} onChange={handleChange} />
+//       </label>
+//       <label>
+//         Фамилия
+//         <input
+//           type="text"
+//           name="secondName"
+//           value={secondName}
+//           onChange={handleChange}
+//         />
+//       </label>
 
-  render() {
-    return (
-      //  сабмит формы обьязательно надо вешать на саму форму
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Имя{' '}
-          <input
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Фамилия
-          <input
-            type="text"
-            name="secondName"
-            value={this.state.secondName}
-            onChange={this.handleChange}
-          />
-        </label>
+//       {/* Делаем радио кнопки */}
+//       <p>Ваш уровень:</p>
+//       <label>
+//         <input
+//           type="radio"
+//           name="experience"
+//           value="Junior"
+//           onChange={handleChange}
+//           checked={experience === 'Junior'}
+//         />
+//         Junior
+//       </label>
+//       <label>
+//         <input
+//           type="radio"
+//           name="experience"
+//           value="Middle"
+//           onChange={handleChange}
+//           checked={experience === 'Middle'}
+//         />
+//         Middle
+//       </label>
+//       <label>
+//         <input
+//           type="radio"
+//           name="experience"
+//           value="Senior"
+//           onChange={handleChange}
+//           checked={experience === 'Senior'}
+//         />
+//         Senior
+//       </label>
+//       <br />
 
-        {/* Делаем радио кнопки */}
-        <p>Ваш уровень:</p>
-        <label>
-          <input
-            type="radio"
-            name="experience"
-            value="Junior"
-            onChange={this.handleChange}
-            checked={this.state.experience === 'Junior'}
-          />
-          Junior
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="experience"
-            value="Middle"
-            onChange={this.handleChange}
-            checked={this.state.experience === 'Middle'}
-          />
-          Middle
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="experience"
-            value="Senior"
-            onChange={this.handleChange}
-            checked={this.state.experience === 'Senior'}
-          />
-          Senior
-        </label>
-        <br />
+//       {/* делаем чекбокс */}
+//       <label>
+//         <input
+//           type="checkbox"
+//           name="licence"
+//           checked={licence}
+//           onChange={this.handleLicence}
+//         />
+//         Подтвердить условиe
+//       </label>
 
-        {/* делаем чекбокс */}
-        <label>
-          <input
-            type="checkbox"
-            name="licence"
-            checked={this.state.licence}
-            onChange={this.handleLicence}
-          />
-          Подтвердить условиe
-        </label>
-
-        {/* кнопка отправки формы. делаем ее неактивной если чекбокс не выбран через disabled */}
-        <button type="submit" disabled={!this.state.licence}>
-          Отправить
-        </button>
-      </form>
-    );
-  }
-}
-export default Form;
+//       {/* кнопка отправки формы. делаем ее неактивной если чекбокс не выбран через disabled */}
+//       <button type="submit" disabled={!licence}>
+//         Отправить
+//       </button>
+//     </form>
+//   );
+// }
